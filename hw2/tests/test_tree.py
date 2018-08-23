3c0e76ec91d29052934e7f7018bc4dcfdc50bc37
EC2 Default User <ec2-user@ip-172-31-37-249.us-east-2.compute.internal>
2018-08-23 21:12:39 +0000

    add test file

--- hw2/tests/test_tree.py
+++ hw2/tests/test_tree.py
@@ -1,44 +1,46 @@
 import unittest
-
 from tree.tree import Tree
 from tree.tree import Node
+
 
 class test_tree(unittest.TestCase):
     def test_empty_tree(self):
         node = None
         self.tree = Tree(node)
         self.answer = "The tree is empty!"
-        assert self.tree.printTree() == self.answer
-       
+        self.assertEqual(self.tree.print_tree(), self.answer)
 
-    def test_case_1(self):   
+    def test_case_1(self):
         node = Node(1, None, None)
         self.tree = Tree(node)
         self.answer = [[1]]
-        assert self.tree.printTree() == self.answer
+        assert self.tree.print_tree() == self.answer
 
     def test_case_2(self):
         node = Node(1, Node(2, Node(3, Node(4, None, None), None), None), None)
         self.tree = Tree(node)
-        self.answer = [['|', '|', '|', '|', '|', '|', '|',  1 , '|', '|', '|', '|', '|', '|', '|'],
-                       ['|', '|', '|',  2 , '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|'],
-                       ['|',  3 , '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|'],
-                       [ 4 , '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|']]
-        assert self.tree.printTree() == self.answer
+        self.answer = [['|', '|', '|', '|', '|', '|', '|', 1, '|', '|', '|', '|', '|', '|', '|'],
+                       ['|', '|', '|', 2, '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|'],
+                       ['|', 3, '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|'],
+                       [4, '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|']]
+        assert self.tree.print_tree() == self.answer
 
     def test_case_3(self):
-        node = Node(1, Node(2, Node(4, None, None), Node(5, None, None)), Node(3, Node(6, None, None), Node(7, None, None)))
+        node = Node(1, Node(2, Node(4, None, None), Node(5, None, None)),
+                    Node(3, Node(6, None, None), Node(7, None, None)))
         self.tree = Tree(node)
-        self.answer = [['|', '|', '|',  1 , '|', '|', '|'],
-                       ['|',  2 , '|', '|', '|',  3 , '|'],
-                       [ 4 , '|',  5 , '|',  6 , '|',  7 ]]
-        assert self.tree.printTree() == self.answer
+        self.answer = [['|', '|', '|', 1, '|', '|', '|'],
+                       ['|', 2, '|', '|', '|', 3, '|'],
+                       [4, '|', 5, '|', 6, '|', 7]]
+        assert self.tree.print_tree() == self.answer
 
     def test_case_4(self):
-        node = Node(1, Node(2, None, Node(4, None, None)), Node(3, Node(5, None, Node(6, None, None)), None))
+        left_node = Node(2, None, Node(4, None, None))
+        right_node = Node(3, Node(5, None, Node(6, None, None)), None)
+        node = Node(1, left_node, right_node)
         self.tree = Tree(node)
-        self.answer = [['|', '|', '|', '|', '|', '|', '|',  1 , '|', '|', '|', '|', '|', '|', '|'],
-                       ['|', '|', '|',  2 , '|', '|', '|', '|', '|', '|', '|',  3 , '|', '|', '|'],
-                       ['|', '|', '|', '|', '|',  4 , '|', '|', '|',  5 , '|', '|', '|', '|', '|'],
-                       ['|', '|', '|', '|', '|', '|', '|', '|', '|', '|',  6 , '|', '|', '|', '|']]
-        assert self.tree.printTree() == self.answer
+        self.answer = [['|', '|', '|', '|', '|', '|', '|', 1, '|', '|', '|', '|', '|', '|', '|'],
+                       ['|', '|', '|', 2, '|', '|', '|', '|', '|', '|', '|', 3, '|', '|', '|'],
+                       ['|', '|', '|', '|', '|', 4, '|', '|', '|', 5, '|', '|', '|', '|', '|'],
+                       ['|', '|', '|', '|', '|', '|', '|', '|', '|', '|', 6, '|', '|', '|', '|']]
+        assert self.tree.print_tree() == self.answer
