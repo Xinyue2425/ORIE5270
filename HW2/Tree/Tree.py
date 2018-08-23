e89a3e9e4fd9d97f4c44c96441739d93e248be0b
EC2 Default User <ec2-user@ip-172-31-37-249.us-east-2.compute.internal>
2018-08-23 13:40:14 +0000

    HW2

---
+++ HW2/Tree/Tree.py
@@ -0,0 +1,80 @@
+from collections import deque
+import numpy as np
+class Tree(object):
+    def __init__(self, root):
+        self.root = root
+        
+    def get_value_root(self):
+        if self.root is not None:
+            return self.root.value
+        else:
+            return None
+    def printTree(Tree):
+        root = Tree.root
+        level_list = []     # to store each level
+        my_deque = deque()  # create a deque to store the node
+        value_list = []     # store the value of node
+        if root is None:
+            return ("The tree is empty!") 
+        else:
+            my_deque.append(root)
+            current_level=1
+            next_level = 0
+            while current_level != 0:
+                node = my_deque.popleft()
+                if node is not None:
+                    value_list.append(node.value)
+                
+                    for child in [node.left, node.right]:
+                        if child is not None:
+                            my_deque.append(child)
+                        else:
+                            my_deque.append(None)  
+                else:
+                    my_deque.append(None)
+                    my_deque.append(None)
+                    value_list.append('|')
+       
+                current_level -= 1   
+                next_level +=2
+                
+                if current_level == 0:
+                    level_list.append(value_list)
+                    value_list = []
+                    current_level = next_level
+                    next_level =  0
+                    if all(item == None for item in my_deque):
+                        break
+    
+        level_list = Level_Traverse(Tree)
+        height = len(level_list)
+        new_list=[]
+        count = 2**(height-1)-1 # count = 7
+        space_list = ["|" for i in range(count)]
+ 
+        new_list.append(space_list+level_list[0]+space_list)
+        for i in range(1,height):
+            temp_list=[]
+            for j in level_list[i][:-1]:
+                temp_list.append(j)
+                temp_list = temp_list + ["|" for i in range(count)]
+            temp_list.append(level_list[i][-1])
+            count = int(count/2)
+            new_list.append(temp_list)
+        final_list=[]
+        for i in new_list:
+            if len(i) != 2**(height)-1:
+                num = int((2**(height)-1 - len(i))/2)
+                space_list = ["|" for i in range(num)]
+                i = space_list + i + space_list
+            final_list.append(i)
+        #length = len(sorted(final_list,key=len, reverse=True)[0])
+        #final_output=np.matrix([xi+[None]*(length-len(xi)) for xi in final_list])
+        return final_list
+           
+
+class Node(object):
+    def __init__(self, value, left, right):
+        self.value = value
+        self.left = left
+        self.right = right
