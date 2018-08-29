778908e8ca1553ed69666f9105d7198b53f43546
EC2 Default User <ec2-user@ip-172-31-37-249.us-east-2.compute.internal>
2018-08-29 17:53:13 +0000

    HW3

---
+++ hw3/negative_cycle/test_negative_cycle.py
@@ -0,0 +1,19 @@
+import unittest
+from negative_cycle.py import detect_negative_cycle
+
+class test_negative_cycle(unittest.TestCase):
+    def test_case1(self):
+        answer = ['There is a negative cycle', ['7', '8', '5']]
+        assert(detect_negative_cycle("graph1.txt")==answer)
+        
+    def test_case2(self):
+        answer = "There is no negative cycle!"
+        assert(detect_negative_cycle("graph2.txt")==answer)
+    
+    def test_case3(self):
+        answer = ['There is a negative cycle', ['2', '1']]
+        assert(detect_negative_cycle("graph3.txt")==answer)
+        
+
+if __name__ == "__main__":
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
