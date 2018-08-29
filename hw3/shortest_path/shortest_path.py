32d152fc451c46c6cf365f7723b1e3319b1397e5
EC2 Default User <ec2-user@ip-172-31-37-249.us-east-2.compute.internal>
2018-08-29 18:30:01 +0000

    HW3

---
+++ hw3/shortest_path/shortest_path.py
@@ -0,0 +1,80 @@
+import numpy as np
+def find_shortest_path(name_txt_file, source, destination):  # graph is a dictionary
+    """
+    read from txt file to get the graph info and find the shortest path using Dijkstra algo
+    :param name_txt_file: 
+    :param source: 
+    :param destination: 
+    :return: 
+    """
+    f = open(name_txt_file, 'r')
+    graph_def = f.read()
+    graph_def= graph_def.split("\n")
+    i=1
+    while(i<len(graph_def)):
+        if graph_def[i] !="":
+            graph_def[i] = eval(graph_def[i])
+            if type(graph_def[i][0]) is not tuple:
+                graph_def[i]=list(graph_def[i])
+                graph_def[i][0]=str(graph_def[i][0])
+                graph_def[i]=[tuple(graph_def[i])]
+            else:
+                temp_list=[]
+                for item in graph_def[i]:
+                    item = list(item)
+                    item[0]=str(item[0])
+                    item = tuple(item)
+                    temp_list.append(item)
+                graph_def[i]=temp_list    
+        else:
+            graph_def[i]=[]
+        i=i+2
+
+    graph_dict={}
+    i=0
+    while (i<len(graph_def)):  # 0,2,4,6,8,10,12,14
+        graph_dict.update({graph_def[i] : graph_def[i+1]})
+        i=i+2
+    
+    if source not in graph_dict.keys() or destination not in graph_dict.keys():
+        return ("Invalid input!")
+    else:
+        F=[]
+        distance= {}   # create a dictionary to record distance from source to each key
+        for i in graph_dict.keys():
+            distance.update({i:0})
+
+        if source == destination:
+            return ("The destination is the same as the source!")
+        else:
+            for neighbor in graph_dict[source]:
+                if destination == neighbor[0]:
+                    return ([source,destination],neighbor[1] )
+            else:
+                for neighbor in graph_dict[source]: # ('2', 4), ('3', 4)
+                    distance[neighbor[0]] = distance[neighbor[0]] + neighbor[1]
+                    F.append(neighbor[0])
+                S=[source]
+
+                while (F!=[]):
+                    min_value = np.inf
+                    for i in F: # "2", "3"
+                        if  (distance[i] <= min_value):
+                            f = i
+                    F.remove(f)
+                    S.append(f)
+                    if f==destination:
+                        break
+                    for neighbor in graph_dict[f]: # ('5', 4), ('6', -2)
+                        if neighbor[0] not in S or neighbor[0] not in F:
+                            distance[neighbor[0]] = distance[f] + neighbor[1]  
+                            #print(distance)
+                            # add the weight to the distance
+                            F.append(neighbor[0])
+                        else:
+                            if (distance[f] + neighbor[1] < distance[neighbor[0]]):
+                                distance[neighbor[0]]= distance[f] + neighbor[1];
+                if destination not in S:
+                    return("We can not reach to destination from source!")
+                else:
+                    return (S, distance[f])
---
+++ hw3/shortest_path/test_shortest_path.py
@@ -0,0 +1,23 @@
+import unittest
+#from negative_cycle.py import detect_negative_cycle
+
+class test_find_shortest_path(unittest.TestCase):
+    def test_case1(self):
+        answer = "The destination is the same as the source!"
+        assert(find_shortest_path("graph_dij.txt", "1", "1")==answer)
+        
+    def test_case2(self):
+        answer = (['1', '2'], 4)
+        assert(find_shortest_path("graph_dij.txt", "1", "2")==answer)
+    
+    def test_case3(self):
+        answer = (['1', '3', '6', '5', '7'], 3)
+        assert(find_shortest_path("graph_dij.txt", "1", "7")==answer)
+        
+    def test_case3(self):
+        answer = "We can not reach to destination from source!"
+        assert(find_shortest_path("graph_dij.txt", "4", "1")==answer)   
+        
+
+if __name__ == "__main__":
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
